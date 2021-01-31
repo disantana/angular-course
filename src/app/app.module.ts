28255b0d3154821440ea28bc4cfaf565b496f053
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2021-01-30 21:07:15 -0300

    feat: Add a Shopping List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { RecipeComponent } from './recipes/recipe/recipe.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component
     RecipeComponent,
     RecipeListComponent,
     RecipeDetailComponent,
-    RecipeItemComponent
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
