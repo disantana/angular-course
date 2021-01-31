dc680b3b736eb03398395fd166b1e9f4e7ccafff
Diego de Santana Pereira <diego.spereira-ext@viavarejo.com.br>
2021-01-31 00:19:42 -0300

    feat: Customize a recipe list page with new recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe kaoskas oasoaksoaks',
+      'This is a recipe test kakajsk udjejd dllsl slslsl',
+      'https://www.handletheheat.com/wp-content/uploads/2015/03/Best-Birthday-Cake-with-milk-chocolate-buttercream-SQUARE-550x550.jpg'),
+    new Recipe('A test recipe kaoskas oasoaksoaks',
+      'This is a recipe test kakajsk udjejd dllsl slslsl',
+      'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2018/12/healthy-breakfast-pizzas.jpg?resize=1024%2C750&ssl=1')
+  ];
   constructor() { }
 
   ngOnInit(): void {
